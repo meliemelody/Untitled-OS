b3f857e32879e29addac74642193bb09017c524b
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-08-23 16:32:38 +0200

    remove unnecessary stuff

--- compile.bat
+++ compile.bat
@@ -7,7 +7,6 @@ C:\i686-elf-tools-windows\bin\i686-elf-as.exe boot.s -o boot.o
 
 C:\i686-elf-tools-windows\bin\i686-elf-gcc.exe -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 C:\i686-elf-tools-windows\bin\i686-elf-gcc.exe -T linker.ld -o isodir/boot/fakyoy.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-cp grub.cfg isodir/boot/grub/grub.cfg
 
 C:\i686-elf-tools-windows\grub\grub-file.exe --is-x86-multiboot isodir/boot/fakyoy.bin
 
