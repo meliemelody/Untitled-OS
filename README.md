74d3f4a6596346117076e7b481015056cc6ba3d5
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-08-23 16:38:30 +0200

    update script, and readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Untitled OS
+
+https://github.com/lordmilko/i686-elf-tools
+https://www.qemu.org/
+https://www.gnu.org/software/grub/grub-download.html
  
--- compile.bat
+++ compile.bat
@@ -1,13 +1,15 @@
 @ECHO OFF
 
+set directory=C:\i686-elf-tools-windows
+
 echo Compiler
 echo     
 
-C:\i686-elf-tools-windows\bin\i686-elf-as.exe kernel/boot.s -o kernel/boot.o
+%directory%\bin\i686-elf-as.exe kernel/boot.s -o kernel/boot.o
 
-C:\i686-elf-tools-windows\bin\i686-elf-gcc.exe -c kernel/kernel.c -o kernel/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-C:\i686-elf-tools-windows\bin\i686-elf-gcc.exe -T linker.ld -o isodir/boot/fakyoy.bin -ffreestanding -O2 -nostdlib kernel/boot.o kernel/kernel.o -lgcc
+%directory%\bin\i686-elf-gcc.exe -c kernel/kernel.c -o kernel/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+%directory%\bin\i686-elf-gcc.exe -T linker.ld -o isodir/boot/fakyoy.bin -ffreestanding -O2 -nostdlib kernel/boot.o kernel/kernel.o -lgcc
 
-C:\i686-elf-tools-windows\grub\grub-file.exe --is-x86-multiboot isodir/boot/fakyoy.bin
+%directory%\grub\grub-file.exe --is-x86-multiboot isodir/boot/fakyoy.bin
 
-C:\i686-elf-tools-windows\qemu\qemu-system-i386.exe -kernel isodir/boot/fakyoy.bin
+%directory%\qemu\qemu-system-i386.exe -kernel isodir/boot/fakyoy.bin
