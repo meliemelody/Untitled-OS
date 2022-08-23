16563701c6b97e7402b819c4c35c2a3fe86b7b9b
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-08-23 16:34:01 +0200

    organize folders

--- compile.bat
+++ compile.bat
@@ -3,10 +3,10 @@
 echo Compiler
 echo     
 
-C:\i686-elf-tools-windows\bin\i686-elf-as.exe boot.s -o boot.o
+C:\i686-elf-tools-windows\bin\i686-elf-as.exe kernel/boot.s -o kernel/boot.o
 
-C:\i686-elf-tools-windows\bin\i686-elf-gcc.exe -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-C:\i686-elf-tools-windows\bin\i686-elf-gcc.exe -T linker.ld -o isodir/boot/fakyoy.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+C:\i686-elf-tools-windows\bin\i686-elf-gcc.exe -c kernel/kernel.c -o kernel/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+C:\i686-elf-tools-windows\bin\i686-elf-gcc.exe -T linker.ld -o isodir/boot/fakyoy.bin -ffreestanding -O2 -nostdlib kernel/boot.o kernel/kernel.o -lgcc
 
 C:\i686-elf-tools-windows\grub\grub-file.exe --is-x86-multiboot isodir/boot/fakyoy.bin
 
